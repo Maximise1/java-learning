d6f1fb193ec82b307daf2b34e03a2f825ca63fd6
Maximise1 <ill_see@mail.ru>
2026-03-27 12:10:47 -0400

    Added email validation

--- notification-service/src/main/java/ru/aston/hometask/listener/dto/UserEvent.java
+++ notification-service/src/main/java/ru/aston/hometask/listener/dto/UserEvent.java
@@ -3,4 +3,14 @@ package ru.aston.hometask.listener.dto;
 public record UserEvent(
         String email,
         UserEventType event
-) {}
+) {
+    public UserEvent {
+        if (email == null || email.isBlank()) {
+            throw new IllegalArgumentException("Email cannot be null or blank");
+        }
+
+        if (!email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$")) {
+            throw new IllegalArgumentException("Invalid email format: " + email);
+        }
+    }
+}
